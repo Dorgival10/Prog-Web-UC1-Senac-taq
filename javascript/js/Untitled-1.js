74041eeaf973abe44a5cd58e524b26560012a3ca
teste10-b <140459149+teste10-b@users.noreply.github.com>
2023-09-25 21:08:51 -0300

    aula_javascript
    primeira aula de javascript

---
+++ javascript/js/Untitled-1.js
@@ -0,0 +1,17 @@
+  // document.getElementById("txt").innerHTML vserve para inserir msg  do id ou clas criado
+  document.getElementById("txt").innerHTML = "Maria"
+  //console.log = serve para exibir texto..
+  console.log("Ola mundo");
+  // aumentando o tamanho e diminuindo o texto atravez do clique
+  function aumentar(){
+      document.getElementById("txt").style.fontSize = "50px";
+  }
+  function diminuir(){
+      document.getElementById("txt").style.fontSize = "20px";
+  }
+  function acender(){
+    document.getElementById("lampada").src ="img/acessa.jpeg";
+  }
+  function desligar(){
+    document.getElementById("lampada").src = "img/apagada.jpeg";
+  }
